71283086da1440ac12f445419cf8a45bb370601c
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-06-01 23:57:30 +0530

    Basics of any Language

---
+++ Learn_the_basics/Things_to_Know_in_any_language/Data_Types.cpp
@@ -0,0 +1,14 @@
+// int, long, long long, float, double
+// string and getline
+// char
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin >> ch;
+    cout << ch;
+    return 0;
+}
