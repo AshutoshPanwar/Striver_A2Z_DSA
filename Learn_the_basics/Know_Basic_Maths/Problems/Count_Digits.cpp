e061331eed3ce9ed1b9a7d922afd190b04d72787
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-06-02 23:28:18 +0530

    Count Digit's

---
+++ Learn_the_basics/Know_Basic_Maths/Problems/Count_Digits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int count = 0, X = N, dig;
+        while(X > 0){
+            dig = X%10;
+            if(dig != 0 && N % dig == 0){
+                count++;
+            }
+            X = X / 10;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
