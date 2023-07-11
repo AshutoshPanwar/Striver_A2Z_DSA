df7ef4d00cd19c368bf9357ec39e4bedd60f8a65
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-07-12 00:03:27 +0530

    Map Based Hashing

---
+++ Learn_the_basics/Learn_Basic_Hashing/Map_Base_Hashing.cpp
@@ -0,0 +1,26 @@
+// Map Based Hashing Approach.
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        mpp[arr[i]]++;
+    }
+    
+    int q;
+    cin >> q;
+    while(q--){
+        int number;
+        cin >> number;
+        // Fetch
+        cout << mpp[number] << endl;
+    }
+    return 0;
+}
